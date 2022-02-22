e895a3cbe17251cdd348cc83eb5cec64aaee6152
Sebastian Lüchow <sebastianluechow@web.de>
2022-02-22 17:45:00 +0100

    Firts UI idea done

--- app.js
+++ app.js
@@ -58,25 +58,26 @@ const results = document.querySelector("#results");
 const running_score = document.querySelector("#running_score");
 let current_score_player = 0;
 let current_score_computer = 0;
+running_score.textContent = current_score_player + "      :      " + current_score_computer;
 
 
 
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => {
     results.textContent = playRound("Rock");
-    running_score.textContent = current_score_player + " : " + current_score_computer;
+    running_score.textContent = current_score_player + "      :      " + current_score_computer;
 
 });
 const paper = document.querySelector("#paper");
 paper.addEventListener("click", () => {
     results.textContent = playRound("Paper");
-    running_score.textContent = current_score_player + " : " + current_score_computer;
+    running_score.textContent = current_score_player + "      :      " + current_score_computer;
 
 });
 const scissors = document.querySelector("#scissors");
 scissors.addEventListener("click", () => {
     results.textContent = playRound("Scissors");
-    running_score.textContent = current_score_player + " : " + current_score_computer;
+    running_score.textContent = current_score_player + "      :      " + current_score_computer;
 
 });
 
@@ -84,5 +85,5 @@ const new_game = document.querySelector("#new_game");
 new_game.addEventListener("click", () => {
     current_score_player = 0;
     current_score_computer = 0;
-    running_score.textContent = current_score_player + " : " + current_score_computer;
+    running_score.textContent = current_score_player + "      :      " + current_score_computer;
 })
